92cd735b06f2e0469014cc17aa69700a5ddbcec3
yash888@gmail.com <yash888@gmail.com@d7e86a57-8a46-0410-862b-39116e8ba8cc>
2009-11-22 17:38:51 +0000

    adding setup.py and tumblr.py
    git-svn-id: http://django-syncr.googlecode.com/svn/trunk@97 d7e86a57-8a46-0410-862b-39116e8ba8cc

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-syncr',
+    version='0.42',
+    description='Syncronize Django with the web',
+    author='Jesse Legg',
+    author_email='jesse.legg@gmail.com',
+    url='http://code.google.com/p/django-syncr',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
