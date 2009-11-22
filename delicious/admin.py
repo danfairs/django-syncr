e0671c0e31c59ccdae42caca9a720c1bbb98335c
yash888@gmail.com <yash888@gmail.com@d7e86a57-8a46-0410-862b-39116e8ba8cc>
2009-11-22 17:48:49 +0000

    moving all the code into django_syncr
    git-svn-id: http://django-syncr.googlecode.com/svn/trunk@98 d7e86a57-8a46-0410-862b-39116e8ba8cc

--- delicious/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from syncr.delicious.models import Bookmark
-
-class BookmarkAdmin(admin.ModelAdmin):
-	list_display = ('saved_date', 'description', 'extended_info')
-	search_fields = ['description', 'extended_info']
-	date_hierarchy = 'saved_date'
-
-admin.site.register(Bookmark, BookmarkAdmin)
--- readernaut/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from models import Book
-
-class BookAdmin(admin.ModelAdmin):
-    search_fields = ['title', 'author',]
-    list_display = ('created', 'title')
-    
-admin.site.register(Book, BookAdmin)
