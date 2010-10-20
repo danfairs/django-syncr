2593a5f46646a2e030c99e9a52af7e0a4f320b94
Dan Fairs <dan@fezconsulting.com>
2010-10-20 11:20:17 +0100

    Move regex compiles to module level so they're only compiled once

--- syncr/twitter/templatetags/twitterfy.py
+++ syncr/twitter/templatetags/twitterfy.py
@@ -5,21 +5,18 @@ from django import template
 
 register = template.Library()
 
+hashtag_pattern = re.compile(r"(?P<start>.?)#(?P<hashtag>[A-Za-z_]+)(?P<end>.?)")
+user_pattern = re.compile(r"(?P<start>.?)@(?P<user>[A-Za-z0-9_]+)(?P<end>.?)") 
+
 @register.filter(name='twitterfy')
 def twitterfy(tweet):
     
-    # find hashtags
-    pattern = re.compile(r"(?P<start>.?)#(?P<hashtag>[A-Za-z_]+)(?P<end>.?)")
-    
-    # replace with link to search
+    # find hashtags, replace with link to search
     link = r'\g<start>#<a href="http://search.twitter.com/search?q=\g<hashtag>"  title="#\g<hashtag> search Twitter">\g<hashtag></a>\g<end>'
-    text = pattern.sub(link,tweet)
+    text = hashtag_pattern.sub(link,tweet)
     
-    # find usernames
-    pattern = re.compile(r"(?P<start>.?)@(?P<user>[A-Za-z0-9_]+)(?P<end>.?)")
-    
-    # replace with link to profile
+    # find usernames, replace with link to profile
     link = r'\g<start>@<a href="http://twitter.com/\g<user>"  title="#\g<user> on Twitter">\g<user></a>\g<end>'
-    text = pattern.sub(link,tweet)
+    text = user_pattern.sub(link,tweet)
     
     return mark_safe(text)
