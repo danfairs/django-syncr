7fdd82e3309743c43ce82558b9ee9d277887d32d
Dan Fairs <dan@fezconsulting.com>
2010-10-20 14:50:29 +0100

    Fix problems parsing the content of the tweets

--- syncr/twitter/templatetags/twitterfy.py
+++ syncr/twitter/templatetags/twitterfy.py
@@ -11,17 +11,16 @@ link_pattern = re.compile(r'(?P<start>.?)(?P<url>https?://[\w\-\.\&\?\/]+)(?P<en
 
 @register.filter(name='twitterfy')
 def twitterfy(tweet):
+    # find links, make them real links
+    link = r'\g<start><a href="\g<url>" title="\g<url>">\g<url></a>\g<end>'
+    tweet = link_pattern.sub(link, tweet)
     
     # find hashtags, replace with link to search
     link = r'\g<start>#<a href="http://search.twitter.com/search?q=\g<hashtag>"  title="#\g<hashtag> search Twitter">\g<hashtag></a>\g<end>'
-    text = hashtag_pattern.sub(link,tweet)
+    tweet = hashtag_pattern.sub(link,tweet)
     
     # find usernames, replace with link to profile
-    link = r'\g<start>@<a href="http://twitter.com/\g<user>"  title="#\g<user> on Twitter">\g<user></a>\g<end>'
-    text = user_pattern.sub(link,tweet)
-
-    # find links, make them real links
-    link = r'\g<start><a href="\g<url>" title="\g<url>">\g<url></a>\g<end>'
-    text = link_pattern.sub(link, tweet)
+    link = r'\g<start><a href="http://twitter.com/\g<user>"  title="#\g<user> on Twitter">@\g<user></a>\g<end>'
+    tweet = user_pattern.sub(link,tweet)
     
-    return mark_safe(text)
+    return mark_safe(tweet)
